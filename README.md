d78af6c38f5c8c955c4e53acaf3baacb125bc12f
Zach Denton <z@chdenton.com>
2018-12-15 22:47:48 -0500

    Add hterm theme.

--- README.md
+++ README.md
@@ -6,27 +6,35 @@ Inspired by [Zenburn](https://github.com/jnurmine/Zenburn) and
 [Dim](https://github.com/jeffkreeftmeijer/vim-dim).
 
 ```
+; black
 let g:terminal_color_0 = '#313633'
 let g:terminal_color_8 = '#2e3330'
 
+; red
 let g:terminal_color_1 = '#dca3a3'
 let g:terminal_color_9 = '#ffcfaf'
 
+; green
 let g:terminal_color_2 = '#7f9f7f'
 let g:terminal_color_10 = '#dfdfbf'
 
+; yellow
 let g:terminal_color_3 = '#e3ceab'
 let g:terminal_color_11 = '#efdcbc'
 
+; blue
 let g:terminal_color_4 = '#c0bed1'
 let g:terminal_color_12 = '#8c8cbc'
 
+; magenta
 let g:terminal_color_5 = '#bca3a3'
 let g:terminal_color_13 = '#cc9393'
 
+; cyan
 let g:terminal_color_6 = '#9fafaf'
 let g:terminal_color_14 = '#8cd0d3'
 
+; white
 let g:terminal_color_7 = '#f0dfaf'
 let g:terminal_color_15 = '#efef8f'
 
---
+++ hterm.js
@@ -0,0 +1,26 @@
+black       = '#313633';
+red         = '#dca3a3'; // red
+green       = '#7f9f7f'; // green
+yellow      = '#e3ceab'; // yellow
+blue        = '#c0bed1'; // blue
+magenta     = '#bca3a3'; // pink
+cyan        = '#9fafaf'; // cyan
+white       = '#f0dfaf'; // light gray
+lightBlack  = '#2e3330'; // medium gray
+lightRed    = '#ffcfaf'; // red
+lightGreen  = '#dfdfbf'; // green
+lightYellow = '#efdcbc'; // yellow
+lightBlue   = '#8c8cbc'; // blue
+lightMagenta= '#cc9393'; // pink
+lightCyan   = '#8cd0d3'; // cyan
+lightWhite  = '#efef8f'; // white
+
+t.prefs_.set('color-palette-overrides',
+  [ black , red     , green  , yellow,
+    blue     , magenta , cyan   , white,
+    lightBlack   , lightRed  , lightGreen , lightYellow,
+    lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+
+t.prefs_.set('cursor-color', '#8faf9f');
+t.prefs_.set('foreground-color', '#dcdccc');
+t.prefs_.set('background-color', '#3f3f3f');
